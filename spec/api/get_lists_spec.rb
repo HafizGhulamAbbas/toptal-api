68112d873bd5a33310e61f048463607dd816ab29
HafizGhulamAbbas <hgabbas1122@gmail.com>
2022-09-09 18:03:20 +0500

    refactor code: added shared_examples

--- spec/api/get_lists_spec.rb
+++ spec/api/get_lists_spec.rb
@@ -1,23 +1,21 @@
+RSpec.shared_examples 'shared test cases' do
+    it 'returns 200 status code' do
+    end
+
+    it 'responds within two seconds' do
+    end
+  end
+
 RSpec.describe 'Todo Lists' do
 
     context "get all lists" do
-        it 'returns 200 status code' do
-        end
-
-        it 'responds within two seconds' do
-        end
-
+        include_examples 'shared test cases'
         it 'contains valid schema' do # [{"name":"Groceries","items":[{"label":"Bread","completed":false},{"label":"Chese","completed":true},{"label":"Tomatoes","completed":false}]},{"name":"Work","items":[{"label":"Code reviews","completed":false},{"label":"DB migration","completed":true},{"label":"Documentation writing","completed":false}]}]
         end
     end
 
     context "adding new list" do
-        it 'returns 200 status code' do
-        end
-
-        it 'responds within two seconds' do
-        end
-
+        include_examples 'shared test cases'
         it 'returns valid schema' do # it includes name and items
         end
 
@@ -26,12 +24,7 @@ RSpec.describe 'Todo Lists' do
     end
 
     context "deleting a list" do
-        it 'returns 200 status code' do
-        end
-
-        it 'responds within two seconds' do
-        end
-
+        include_examples 'shared test cases'
         it 'returns valid schema' do # it includes name and items
         end
 
@@ -40,12 +33,7 @@ RSpec.describe 'Todo Lists' do
     end
 
     context "adding new list item" do
-        it 'returns 200 status code' do
-        end
-
-        it 'responds within two seconds' do
-        end
-
+        include_examples 'shared test cases'
         it 'returns valid schema' do # {"label":"Bread","completed":true}
         end
 
@@ -54,34 +42,19 @@ RSpec.describe 'Todo Lists' do
     end
 
     context "marking a list item as COMPLETE" do
-        it 'returns 200 status code' do
-        end
-
-        it 'responds within two seconds' do
-        end
-
+        include_examples 'shared test cases'
         it 'returns valid schema' do # {"label":"Bread","completed":true}
         end
     end
 
     context "marking a list item as INCOMPLETE" do
-        it 'returns 200 status code' do
-        end
-
-        it 'responds within two seconds' do
-        end
-
+        include_examples 'shared test cases'
         it 'returns valid schema' do # {"label":"Bread","completed":true}
         end
     end
 
     context "deleting a list item" do  
-        it 'returns 200 status code' do
-        end
-
-        it 'responds within two seconds' do
-        end
-
+        include_examples 'shared test cases'
         it 'returns valid schema' do # [{"label":"tomatoes","completed":false},{"label":"bread","completed":false}]
         end
 
@@ -90,12 +63,7 @@ RSpec.describe 'Todo Lists' do
     end
 
     context "overwrite the database" do
-        it 'returns 200 status code' do
-        end
-
-        it 'responds within two seconds' do
-        end
-
+        include_examples 'shared test cases'
         it 'returns valid schema' do # [{"name":"L1","items":[{"label":"L11","completed":false},{"label":"L12","completed":true}]}]
         end
 
