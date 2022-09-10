39f4c3e96829b407640307238db0dba6833e3ef1
HafizGhulamAbbas <hgabbas1122@gmail.com>
2022-09-10 14:39:32 +0500

    Added test case: List names are unique

--- spec/api/get_lists_spec.rb
+++ spec/api/get_lists_spec.rb
@@ -1,74 +1,22 @@
 RSpec.shared_examples 'shared test cases' do
-    it 'returns 200 status code' do
-    end
-
-    it 'responds within two seconds' do
-    end
+    # it 'returns 200 status code' do
+    #     expect(response_status).to eq(200)
+    # end
   end
 
 RSpec.describe 'Todo Lists' do
 
-    context "get all lists" do
-        include_examples 'shared test cases'
-        it 'contains valid schema' do # [{"name":"Groceries","items":[{"label":"Bread","completed":false},{"label":"Chese","completed":true},{"label":"Tomatoes","completed":false}]},{"name":"Work","items":[{"label":"Code reviews","completed":false},{"label":"DB migration","completed":true},{"label":"Documentation writing","completed":false}]}]
+    context "list names are unique" do
+        BASE_URL = "http://localhost:4567"
+        list_name = Time.now
+        new_list_response = Faraday.new(url: BASE_URL).post('/lists/add', {"name": list_name}.to_json)
+        duplicate_list_response = Faraday.new(url: BASE_URL).post('/lists/add', {"name": list_name}.to_json)
+        
+        let(:new_list) { JSON.parse(new_list_response.body)["name"] }
+        let(:duplicate_list) { JSON.parse(duplicate_list_response.body)["name"] }
+       
+        it 'retricts duplicate list name' do
+            expect(new_list).not_to eq(duplicate_list)
         end
-    end
-
-    context "adding new list" do
-        include_examples 'shared test cases'
-        it 'returns valid schema' do # it includes name and items
-        end
-
-        it 'increases the list count by 1' do
-        end
-    end
-
-    context "deleting a list" do
-        include_examples 'shared test cases'
-        it 'returns valid schema' do # it includes name and items
-        end
-
-        it 'decreases the list count by 1' do
-        end
-    end
-
-    context "adding new list item" do
-        include_examples 'shared test cases'
-        it 'returns valid schema' do # {"label":"Bread","completed":true}
-        end
-
-        it 'should have default status INCOMPLETE' do
-        end
-    end
-
-    context "marking a list item as COMPLETE" do
-        include_examples 'shared test cases'
-        it 'returns valid schema' do # {"label":"Bread","completed":true}
-        end
-    end
-
-    context "marking a list item as INCOMPLETE" do
-        include_examples 'shared test cases'
-        it 'returns valid schema' do # {"label":"Bread","completed":true}
-        end
-    end
-
-    context "deleting a list item" do  
-        include_examples 'shared test cases'
-        it 'returns valid schema' do # [{"label":"tomatoes","completed":false},{"label":"bread","completed":false}]
-        end
-
-        it 'decreases the list items count by 1' do
-        end
-    end
-
-    context "overwrite the database" do
-        include_examples 'shared test cases'
-        it 'returns valid schema' do # [{"name":"L1","items":[{"label":"L11","completed":false},{"label":"L12","completed":true}]}]
-        end
-
-        context "newly added database"
-            it 'has the correct data count' do
-            end
     end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@
 
 require "faraday"
 require "json"
+require "set"
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
