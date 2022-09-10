23055985a3baa93219a2b75659e483a09d236ba3
HafizGhulamAbbas <hgabbas1122@gmail.com>
2022-09-10 14:41:35 +0500

    Added test case: api should response within 200ms

--- spec/api/get_lists_spec.rb
+++ spec/api/get_lists_spec.rb
@@ -2,6 +2,10 @@ RSpec.shared_examples 'shared test cases' do
     # it 'returns 200 status code' do
     #     expect(response_status).to eq(200)
     # end
+
+    it 'responds within 200ms' do
+        expect(time_taken*100).to be < 200
+    end
   end
 
 RSpec.describe 'Todo Lists' do
@@ -18,5 +22,21 @@ RSpec.describe 'Todo Lists' do
         it 'retricts duplicate list name' do
             expect(new_list).not_to eq(duplicate_list)
         end
+    end
+
+    context "api should response within 200ms" do
+        let(:response_with_time) do
+            start_time = Time.now
+            response = Faraday.new(url: BASE_URL).post 'list/0/item/0/complete/true' do |req|
+                req.headers[:content_type] = 'application/json'
+            end
+            end_time = Time.now
+            response_time = end_time - start_time
+            [response, response_time]
+        end
+
+        let(:time_taken) {response_with_time[1]}
+
+        include_examples 'shared test cases'
     end
 end
